9f54c9f2d37bd2ba877ce94b2bc0a10f17ca28ef
Austin Songer <a.songer@protonmail.com>
2021-04-18 23:14:21 +0000

    Update

--- 3rd-Party-Submodules/blackip/bipupdate/tools/debugbip.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-a=set(line.strip().lower() for line in open('blackip.txt').readlines())
-b=set(line.strip().lower() for line in open('debugip').readlines())
-open("outip", "w").write("\n".join(a.difference(b)))
--- 3rd-Party-Submodules/blackweb/bwupdate/tools/debug_error.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-a=set(line.strip().lower() for line in open('blackweb.txt').readlines())
-b=set(line.strip().lower() for line in open('SquidError.txt').readlines())
-open("final", "w").write("\n".join(a.difference(b)))
